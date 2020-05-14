a3bc74fc19d1be80c21b1ec403d4578b8b6394a8
Rodion Sibov <rodionsibov@gmail.com>
2020-05-14 09:01:45 +0100

    canvas clock numbers

--- exercises.js
+++ exercises.js
@@ -829,80 +829,109 @@ console.log(nextInLine('1, 2, 3, 4, 5,', 6))
 // divEl.classList.add('penguin');
 // document.body.insertBefore(divEl, document.body.childNodes[penguinsEl.length]);
 
+// const calculateMax = (coll) => {
+
+//     if (coll.length === 0) {
+// 	return null;
+//     }
+
+//     let max = coll[0];
+//     for (let i = 1; i < coll.length; i += 1) {
+// 	const currentElement = coll[i];
+// 	if (currentElement > max) {
+// 	    max = currentElement;
+// 	}
+//     }
+//     return max;
+// };
+
+// console.log(calculateMax([3, 2, -10, 38, 0]));
 
 
 //Canvas Clock
   
-// const divEl = document.createElement('div');
-// divEl.innerHTML = `<canvas id="myCanvas" width="400" height="400" style="background-color: #333;"></canvas>`;
-// document.body.insertBefore(divEl, document.body.childNodes[2]);
-// const canvas = document.getElementById('myCanvas');
-// const ctx = canvas.getContext('2d');
-// let radius = canvas.height / 2;
-// console.log(canvas.height)
-// ctx.translate(radius, radius);
-// radius = radius * 0.90;
-// drawClock();
+const divEl = document.createElement('div');
+divEl.innerHTML = `<canvas id="myCanvas" width="400" height="400" style="background-color: #333;"></canvas>`;
+document.body.insertBefore(divEl, document.body.childNodes[2]);
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+let radius = canvas.height / 2;
+console.log(canvas.height)
+ctx.translate(radius, radius);
+radius = radius * 0.90;
+drawClock();
 
-// function drawClock() {
-//     drawFace(ctx, radius);
-//     drawNumbers(ctx, radius);
-// }
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
 
-// function drawNumbers(ctx, radius) {
-//     var ang;
-//     var num;
-//     ctx.font = radius * 0.15 + "px arial";
-//     ctx.textBaseline = "middle";
-//     ctx.textAlign = "center";
-//     for(num = 1; num < 13; num++){
-// 	ang = num * Math.PI / 6;
-// 	ctx.rotate(ang);
-// 	ctx.translate(0, -radius * 0.85);
-// 	ctx.rotate(-ang);
-// 	ctx.fillText(num.toString(), 0, 0);
-// 	ctx.rotate(ang);
-// 	ctx.translate(0, radius * 0.85);
-// 	ctx.rotate(-ang);
-//     }
-// }
+function drawTime(ctx, radius){
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  //hour
+  hour = hour%12;
+  hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+  drawHand(ctx, hour, radius*0.5, radius*0.07);
+  //minute
+  minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+  drawHand(ctx, minute, radius*0.8, radius*0.07);
+  // second
+  second = (second*Math.PI/30);
+  drawHand(ctx, second, radius*0.9, radius*0.02);
+}
 
-// function drawFace(ctx, radius) {
-//     var grad;
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0,0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+}
 
-//     ctx.beginPath();
-//     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-//     ctx.fillStyle = 'white';
-//     ctx.fill();
-
-//     grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
-//     grad.addColorStop(0, '#333');
-//     grad.addColorStop(0.5, 'white');
-//     grad.addColorStop(1, '#333');
-//     ctx.strokeStyle = grad;
-//     ctx.lineWidth = radius*0.1;
-//     ctx.stroke();
-
-//     ctx.beginPath();
-//     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-//     ctx.fillStyle = '#333';
-//     ctx.fill();
-// }
-
-const calculateMax = (coll) => {
-
-    if (coll.length === 0) {
-	return null;
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for(num = 1; num < 13; num++){
+	ang = num * Math.PI / 6;
+	ctx.rotate(ang);
+	ctx.translate(0, -radius * 0.85);
+	ctx.rotate(-ang);
+	ctx.fillText(num.toString(), 0, 0);
+	ctx.rotate(ang);
+	ctx.translate(0, radius * 0.85);
+	ctx.rotate(-ang);
     }
+}
 
-    let max = coll[0];
-    for (let i = 1; i < coll.length; i += 1) {
-	const currentElement = coll[i];
-	if (currentElement > max) {
-	    max = currentElement;
-	}
-    }
-    return max;
-};
+function drawFace(ctx, radius) {
+    var grad;
 
-console.log(calculateMax([3, 2, -10, 38, 0]));
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+
+    grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius*0.1;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
+
