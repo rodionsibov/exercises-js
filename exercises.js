f3373a102015dab406e0c99dfa9dcc9fa7c1cdd0
Rodion Sibov <rodionsibov@gmail.com>
2020-05-19 23:25:20 +0100

    js challenges: bubble sort

--- exercises.js
+++ exercises.js
@@ -1064,3 +1064,26 @@ console.log(nextInLine('1, 2, 3, 4, 5,', 6))
 
 // const cUc = (str) => [...new Set(str.split(''))].length;
 
+const bubbleSort = (items) => {
+    let stepsCount = items.length - 1;
+    let swapped;
+
+    do {
+	swapped = false;
+
+	for (let i = 0; i < stepsCount; i += 1) {
+	    if (items[i] > items[i + 1]) {
+		let temp = items[i];
+		items[i] = items[i + 1];
+		items[i + 1] = temp;
+		swapped = true;
+	    }
+	}
+	stepsCount -= 1;
+    } while (swapped);
+
+    return items;
+}
+
+console.log(bubbleSort([3, 2, 10, -2, 0]));
+
