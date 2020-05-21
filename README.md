c056519c081e3675c377a3c11aee67c69aa878a1
Rodion Sibov <rodionsibov@gmail.com>
2020-05-21 18:35:58 +0100

    update dir

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Exercises JavaScript
 Each exercise starts with a problem description. Read this description and try to solve the exercise.  
 
-__If you want to learn something from the exercises, I recommend looking at the solutions only after you’ve solved the exercise, or at least after you’ve attacked it long and hard enough to have a slight headache.__
+__If we want to learn something from the exercises, let's looking at the solutions only after we’ve solved the exercise, or at least after we’ve attacked it long and hard enough to have a slight headache.__
 
 Good luck!
 
@@ -20,77 +20,4 @@ Good luck!
 - https://coderbyte.com/information/First%20Reverse:JavaScript
 - https://coderbyte.com/editor/frontend:React%20Button%20Toggle
 - https://coderbyte.com/information/SQL%20Member%20Count
-
-
-__Important: You shouldn't use global variables__
-
-1) Write largest and smallest functions that returns the largest and smallest number passed like a argument.
-```
-Example:
-largest(2, 0.1, -5, 100, 3) // 100
-smallest(2, 0.1, -5, 100, 3) // -5
-```
-2) Write function transform that will transform array of numbers to array of functions that will return value from a base array.
-```
-Example:
-const baseArray = [10, 20, 30, 40, 50];
-const newArray = transform(baseArray);
-newArray[3](); // should return 40
-newArray[4](); // should return 50
-```
-3) Write function sum. Function expects arbitrary number of digit arguments and returns compound value of them.
-Note: function should use recursion
-```
-Example:
-sum(1,3,5,7); //should return 16
-```
-4) Write function countDown. Function expects number and logs values one by one till zero with one second delay.
-```
-Example:
-countDown(3); // 3 2 1 0
-```
-5) Write a polyfill for a .bind() function and save it in Function.prototype.myBind(). myBind() should work in an exact same way as the usual bind() - take context as a first parameter and the list of arguments separated by comma.
-Hint: play with the function in Function.prototype and see what this points to inside it. Your code should look like: `Function.prototype.myBind = function () {
-// your code here
-}`
-```
-Example:
-function addPropToNumber(number) { return this.prop + number; }
-var bound = addPropToNumber.myBind({ prop: 9 });
-bound(1) // 10
-```
-6) Write a function splitAndMerge
-Function accept 2 parameters:str and sp. str is a sentence. sp is a char as separator. First we need to divide the sentence into words(Use separator space); and then divide each word into characters(Use separator empty string); and then merge each characters with the specified sp; at last merge all the words(Use separator space) and return it.
-```
-Example:
-splitAndMerge("My name is John"," ") should return "M y n a m e i s J o h n"
-splitAndMerge("Hello World!",",") should return "H,e,l,l,o W,o,r,l,d,!"
-```
-7) Write a function convert
-Convert a hash into an array. Nothing more, Nothing less.
-```{name: 'Jeremy', age: 24, role: 'Software Engineer'}
-should be converted into
-[["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
-```
-8) Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
-```
-Example:
-toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-```
-9) Write a function that takes a sentence (string) and reverses each word in the sentence.
-```
-Example:
-" A fun little challenge! " => " A nuf elttil !egnellahc "
-```
-10) Write a function stringExpansion
-Given a string that includes alphanumeric characters ('3a4B2d') return the expansion of that string: The numeric values represent the occurance of each letter preceding that numeric value. There should be no numeric characters in the final string. Empty strings should return an empty string.
-
-> The first occurance of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears.
-stringExpansion('3D2a5d2f') === 'DDDaadddddff'
-If there are two consecutive numeric characters the first one is ignored.
-stringExpansion('3d332f2a') === 'dddffaa'
-If there are two consecutive alphabetic characters then the first character has no effect on the one after it.
-stringExpansion('abcde') === 'abcde'
-Your code should be able to work for both lower and capital case letters.
 
--- exercises.js
+++ exercises.js
@@ -1212,7 +1212,7 @@ console.log(nextInLine('1, 2, 3, 4, 5,', 6))
 //   'MongoDB'
 // ]
 // let result = '';
-// for (w of webTechs) {
+// for (let w of webTechs) {
 //     if (w.length > result.length) {
 // 	result = w
 //     }
@@ -1237,7 +1237,226 @@ console.log(nextInLine('1, 2, 3, 4, 5,', 6))
 // console.log(result);
 
 
-const stack = ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
-for (s of stack) {
-    console.log(s)
-}
+// const stack = ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+// let result = []
+// for (let s of stack) {
+//     result.unshift(s)
+// }
+// console.log(result)
+
+// const countries = [
+//   'Afghanistan',
+//   'Albania',
+//   'Algeria',
+//   'Andorra',
+//   'Angola',
+//   'Antigua and Barbuda',
+//   'Argentina',
+//   'Armenia',
+//   'Australia',
+//   'Austria',
+//   'Azerbaijan',
+//   'Bahamas',
+//   'Bahrain',
+//   'Bangladesh',
+//   'Barbados',
+//   'Belarus',
+//   'Belgium',
+//   'Belize',
+//   'Benin',
+//   'Bhutan',
+//   'Bolivia',
+//   'Bosnia and Herzegovina',
+//   'Botswana',
+//   'Brazil',
+//   'Brunei',
+//   'Bulgaria',
+//   'Burkina Faso',
+//   'Burundi',
+//   'Cambodia',
+//   'Cameroon',
+//   'Canada',
+//   'Cape Verde',
+//   'Central African Republic',
+//   'Chad',
+//   'Chile',
+//   'China',
+//   'Colombi',
+//   'Comoros',
+//   'Congo (Brazzaville)',
+//   'Congo',
+//   'Costa Rica',
+//   "Cote d'Ivoire",
+//   'Croatia',
+//   'Cuba',
+//   'Cyprus',
+//   'Czech Republic',
+//   'Denmark',
+//   'Djibouti',
+//   'Dominica',
+//   'Dominican Republic',
+//   'East Timor (Timor Timur)',
+//   'Ecuador',
+//   'Egypt',
+//   'El Salvador',
+//   'Equatorial Guinea',
+//   'Eritrea',
+//   'Estonia',
+//   'Ethiopia',
+//   'Fiji',
+//   'Finland',
+//   'France',
+//   'Gabon',
+//   'Gambia, The',
+//   'Georgia',
+//   'Germany',
+//   'Ghana',
+//   'Greece',
+//   'Grenada',
+//   'Guatemala',
+//   'Guinea',
+//   'Guinea-Bissau',
+//   'Guyana',
+//   'Haiti',
+//   'Honduras',
+//   'Hungary',
+//   'Iceland',
+//   'India',
+//   'Indonesia',
+//   'Iran',
+//   'Iraq',
+//   'Ireland',
+//   'Israel',
+//   'Italy',
+//   'Jamaica',
+//   'Japan',
+//   'Jordan',
+//   'Kazakhstan',
+//   'Kenya',
+//   'Kiribati',
+//   'Korea, North',
+//   'Korea, South',
+//   'Kuwait',
+//   'Kyrgyzstan',
+//   'Laos',
+//   'Latvia',
+//   'Lebanon',
+//   'Lesotho',
+//   'Liberia',
+//   'Libya',
+//   'Liechtenstein',
+//   'Lithuania',
+//   'Luxembourg',
+//   'Macedonia',
+//   'Madagascar',
+//   'Malawi',
+//   'Malaysia',
+//   'Maldives',
+//   'Mali',
+//   'Malta',
+//   'Marshall Islands',
+//   'Mauritania',
+//   'Mauritius',
+//   'Mexico',
+//   'Micronesia',
+//   'Moldova',
+//   'Monaco',
+//   'Mongolia',
+//   'Morocco',
+//   'Mozambique',
+//   'Myanmar',
+//   'Namibia',
+//   'Nauru',
+//   'Nepal',
+//   'Netherlands',
+//   'New Zealand',
+//   'Nicaragua',
+//   'Niger',
+//   'Nigeria',
+//   'Norway',
+//   'Oman',
+//   'Pakistan',
+//   'Palau',
+//   'Panama',
+//   'Papua New Guinea',
+//   'Paraguay',
+//   'Peru',
+//   'Philippines',
+//   'Poland',
+//   'Portugal',
+//   'Qatar',
+//   'Romania',
+//   'Russia',
+//   'Rwanda',
+//   'Saint Kitts and Nevis',
+//   'Saint Lucia',
+//   'Saint Vincent',
+//   'Samoa',
+//   'San Marino',
+//   'Sao Tome and Principe',
+//   'Saudi Arabia',
+//   'Senegal',
+//   'Serbia and Montenegro',
+//   'Seychelles',
+//   'Sierra Leone',
+//   'Singapore',
+//   'Slovakia',
+//   'Slovenia',
+//   'Solomon Islands',
+//   'Somalia',
+//   'South Africa',
+//   'Spain',
+//   'Sri Lanka',
+//   'Sudan',
+//   'Suriname',
+//   'Swaziland',
+//   'Sweden',
+//   'Switzerland',
+//   'Syria',
+//   'Taiwan',
+//   'Tajikistan',
+//   'Tanzania',
+//   'Thailand',
+//   'Togo',
+//   'Tonga',
+//   'Trinidad and Tobago',
+//   'Tunisia',
+//   'Turkey',
+//   'Turkmenistan',
+//   'Tuvalu',
+//   'Uganda',
+//   'Ukraine',
+//   'United Arab Emirates',
+//   'United Kingdom',
+//   'United States',
+//   'Uruguay',
+//   'Uzbekistan',
+//   'Vanuatu',
+//   'Vatican City',
+//   'Venezuela',
+//   'Vietnam',
+//   'Yemen',
+//   'Zambia',
+//   'Zimbabwe'
+// ]
+// let containWord = []
+// let result = ''
+// let onlyFour = []
+// let twoOrMoreWords = []
+// let arr = []
+// for (let c of countries) {
+//     if (c.includes('ah')) {
+// 	containWord.push(c)
+//     } else if (c.length > result.length ) {
+// 	result = c
+//     } else if (c.length === 4) {
+// 	onlyFour.push(c)
+//     } else if (c.includes(' ')) {
+// 	twoOrMoreWords.push(c)
+//     } else {
+// 	arr.push(c.toUpperCase())
+// 	arr.reverse()
+//     }
+// }  
+// console.log(arr.indexOf('GHANA'))
+
