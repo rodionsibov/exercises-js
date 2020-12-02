f33c66ae9975854cc52eb157eb7b688d94c5e707
Rodion Sibov <rodionsibov@gmail.com>
2020-12-02 17:18:13 +0200

    first commit

---
+++ day3-exercises.js
@@ -0,0 +1,16 @@
+// const birthYear = prompt('Enter birth year: ');
+// const now = new Date();
+// const yearsOld = now.getFullYear() - birthYear;
+
+// if (yearsOld >= 18) {
+//     alert(`You are ${yearsOld}. You are old enough to drive.`)
+//     console.log(`You are ${yearsOld}. You are old enough to drive.`)
+    
+// } else {
+//     alert(`You are ${yearsOld}. You will be allowed to drive after ${18 - yearsOld} years.`)
+//     console.log(`You are ${yearsOld}. You will be allowed to drive after ${18 - yearsOld} years.`)
+// }
+
+
+
+
