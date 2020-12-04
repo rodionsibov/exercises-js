5863568dd3cd913c018cf1570a04823dd1c364e2
Rodion Sibov <rodionsibov@gmail.com>
2020-12-04 16:57:40 +0200

    Enter your age && isEven

--- day4-exercises.js
+++ day4-exercises.js
@@ -1,1 +1,31 @@
 // Conditionals
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// rl.question('Enter your age: ', (answer) => {
+
+//     if (answer >= 18) {
+// 	console.log(`You are old enough to drive. Your answer: ${answer}`);
+//     } else {
+// 	console.log(`You are left with ${18 - answer} years to drive. Your answer: ${answer}`);
+//     }
+
+//     rl.close();
+// });
+
+
+rl.question('Enter a number: ', (answer) => {
+
+    if (answer % 2 === 0) {
+	console.log(`${answer} is an even number`);
+    } else {
+	console.log(`${answer} is an odd number`);
+    }
+
+    rl.close();
+});
