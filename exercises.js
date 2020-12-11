4f76cd9ea170abf89cf82da1c88aaa5d5c03e925
Rodion Sibov <rodionsibov@gmail.com>
2020-12-11 16:16:55 +0200

    Utils: validations

--- exercises.js
+++ exercises.js
@@ -196,3 +196,37 @@
 
 // const createPerson = (name, age, gender) => ({name, age, gender});
 // console.log(createPerson("Zodiac", 57, "male"));
+
+
+// const digitsum = num => String(num).split('').reduce((a, b) => +a + +b, 0)
+// console.log(digitsum(19642))
+
+
+
+// utils/validations.js
+
+// const isValidEmail = function(email) {
+//   if (/^[^@ ]+@[^@ ]+\.[^@ \.]{2,}$/.test(email)) {
+//     return "email is valid";
+//   } else {
+//     return "email is invalid";
+//   }
+// };
+
+// const isValidPhone = function(phone) {
+//   if (/^[\\(]\d{3}[\\)]\s\d{3}-\d{4}$/.test(phone)) {
+//     return "phone number is valid";
+//   } else {
+//     return "phone number is invalid";
+//   }
+// };
+
+// function isEmpty(value) { 
+//   if (/^\s*$/.test(value)) {
+//     return "string is empty or contains only spaces";
+//   } else {
+//     return "string is not empty and does not contain spaces";
+//   } 
+// }
+
+// export { isValidEmail, isValidPhone, isEmpty };
