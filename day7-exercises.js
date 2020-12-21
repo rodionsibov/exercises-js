493320d5a9a81d2c0e8c20eae87716d87dd562b5
Rodion Sibov <rodionsibov@gmail.com>
2020-12-21 19:21:25 +0200

    Functions reverse array

--- day7-exercises.js
+++ day7-exercises.js
@@ -7,3 +7,14 @@
 //     return {a, b}
 // }
 // console.log(swapValues(1, 7));
+
+// const reverseArray = (...args) => {
+//     const result = [];
+//     for (const el of args) {
+// 	result.unshift(el);
+//     }
+//     return result;
+// }
+// console.log(reverseArray("A", "B", 3, [2234, 2342, {c: "C", d: "D"}, 4234]));
+
+
