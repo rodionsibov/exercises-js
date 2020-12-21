aed125f05bbaf7e8e6bd6303bc47e8146ab5cebb
Rodion Sibov <rodionsibov@gmail.com>
2020-12-21 19:04:14 +0200

    Functions swap values

--- day7-exercises.js
+++ day7-exercises.js
@@ -1,1 +1,9 @@
-// Functions
+// // Functions
+// const swapValues = (a, b) => {
+//     console.log({a, b})
+//     const temp = a;
+//     a = b;
+//     b = temp;
+//     return {a, b}
+// }
+// console.log(swapValues(1, 7));
