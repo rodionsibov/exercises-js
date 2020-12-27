df18976ed01b28cd7a11edf28ab1d1dd516bef33
Rodion Sibov <rodionsibov@gmail.com>
2020-12-27 10:48:52 +0200

    Take oldest. JS functions

--- day8-exercises.js
+++ day8-exercises.js
@@ -1,1 +1,20 @@
 // Objects
+
+
+// const users = [
+//     { name: 'Tirion', birthday: 'Nov 19, 1988' },
+//     { name: 'Sam', birthday: 'Nov 22, 1999' },
+//     { name: 'Rob', birthday: 'Jan 11, 1975' },
+//     { name: 'Sansa', birthday: 'Mar 20, 2001' },
+//     { name: 'Tisha', birthday: 'Feb 27, 1992' },
+//     { name: 'Chris', birthday: 'Dec 25, 1995' },
+// ];
+
+// const takeOldest = (users, count = 1) => {
+//     return users
+// 	.sort((a, b) => Date.parse(a.birthday) - Date.parse(b.birthday))
+// 	.slice(0, count)
+// }
+// console.log(users);
+// console.log(takeOldest(users,  3));
+
