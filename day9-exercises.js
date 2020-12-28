981e567d58cf58683964b2ee8980cbe468efb3cc
Rodion Sibov <rodionsibov@gmail.com>
2020-12-28 22:18:41 +0200

    Higher order functions

--- day9-exercises.js
+++ day9-exercises.js
@@ -1,1 +1,22 @@
 // Higher Order Functions
+
+// const users = [
+//   { name: 'Petr', age: 4 },
+//   { name: 'Igor', age: 19 },
+//   { name: 'Vovan', age: 4 },
+//   { name: 'Matvey', age: 16 },
+// ];
+// const oldest = users.reduce((acc, user) => user.age > acc.age ? user : acc)
+// console.log(oldest)
+
+
+// const numbers = [1,2,4,5];
+// const sumArray = arr => {
+//     let sum = 0;
+//     numbers.forEach(element => {
+// 	sum += element;
+//     })
+//     return sum;
+// }
+// console.log(sumArray(numbers));
+
