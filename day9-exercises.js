b2de0257ca0e330b73ebe9d41241e50e068e5ba8
Rodion Sibov <rodionsibov@gmail.com>
2020-12-30 09:28:11 +0200

    JavaScript testing: throw new Error

--- day9-exercises.js
+++ day9-exercises.js
@@ -43,7 +43,77 @@
 // };
 // console.log(check(32));
 
-const numbers = [1, 2, 3, 4, 5]
-const result = numbers.map((num) => console.log(num * num))
-console.log(result)
+// const users = [
+//     { name: 'Asabeneh', age: 150 },
+//     { name: 'Brook', age: 50 },
+//     { name: 'Eyo', age: 100 },
+//     { name: 'Elias', age: 22 },
+// ]
+// // users.sort((a, b) => {
+// //     if (a.age < b.age) return -1
+// //     if (a.age > b.age) return 1
+// //     return 0
+// // })
+// users.sort((a, b)=> a.age - b.age)
+// console.log(users)
+
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+//countries.forEach(country => console.log(country));
+//names.forEach(name => console.log(name));
+//countries.map(country => console.log(country.toUpperCase()));
+//countries.map(country => console.log(country.length));
+//numbers.map(number => console.log(number ** 2));
+//products.map(({product, price}) => console.log([product, price]));
+//console.log(countries.filter(country => country.includes("land")));
+//console.log(countries.filter(country => country.length >= 6));
+//console.log(countries.filter(country => country.match(/^i/i)))
+//console.log(products.filter(({price}) => typeof price === "number"))
+//console.log(products.filter(({price}) => price > 0))
+// const getStringLists = (arr) => {
+//     return arr.filter(el => typeof el === "string")
+// }
+// const arr = [...names, ...numbers]
+// console.log(JSON.stringify(arr))
+// console.log(getStringLists(arr))
+// console.log(numbers)
+// console.log(numbers.reduce((acc, cur) => acc + cur, 0))
+//console.log(countries.reduce((acc, cur) => `${acc}, ${cur}`), "are north European countries")
+// console.log(names.every(name => name.length > 7))
+// console.log(names.some(name => name.length > 7))
+// console.log(names)
+//console.log(countries.find(country => country.length === 6))
+//console.log(countries.findIndex(country => country.length === 6))
+//console.log(countries.findIndex(country => country === "Norway"))
+// console.log(countries.findIndex(country => country === "Russia"))
+// console.log(countries.includes("Russia"))
+// console.log(countries)
+
+// const capitalize = (str) => {
+//     return `${str[1].toUpperCase()}${str.slice(1)}`
+// }
+
+// const capitalize = (text) => {
+//     if (text === "") {
+// 	return "";
+//     }
+//     const [firstSymbol, ...restSymbols] = text;
+//     return `${firstSymbol.toUpperCase()}${restSymbols.join("")}`
+// }
+
+// if (capitalize("hello") !== "Hello") {
+//     throw new Error("The function works incorrectly")
+// }
+// if (capitalize("") !== "") {
+//     throw new Error("The function works incorrectly")
+// }
 
