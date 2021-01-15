50c97c69f05c1990d339cd820302341e04982944
Rodion Sibov <rodionsibov@gmail.com>
2021-01-16 01:10:18 +0200

    Update

--- day19-exercises.js
+++ day19-exercises.js
@@ -1,2 +1,16 @@
 // Closures
 
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
