bc2c50f17de9866db6ee1c9bf7be0ca9086a5915
Rodion Sibov <rodionsibov@gmail.com>
2021-02-02 17:00:55 +0200

    Update

--- exercises.js
+++ exercises.js
@@ -637,9 +637,23 @@
 // console.log(pairElement("GCG"))
 
 
+// function fearNotLetter(str) {
+//     const arr = str.split("").map(el => el.charCodeAt());
+//     for (let i = 0; i < arr.length - 1; i++) {
+// 	if (arr[i] + 1 !== arr[i + 1]) {
+// 	    return String.fromCharCode(arr[i + 1]);
+// 	}
+//     }
+//     return undefined;
+// }
+// console.log(fearNotLetter("abce")); // d
+// console.log(fearNotLetter("stvwx")); // u
 
-
-
+function uniteUnique(arr) {
+    return [...new Set(arr.flat())];
+}
+console.log(uniteUnique([[1,3,2],[5,2,1,4],[2,1]])) // [1,3,2,5,4]
+console.log(uniteUnique([[1,2,3],[5,2,1]])) // [1,2,3,5]
 
 
 
