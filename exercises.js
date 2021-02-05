8911ca4986411165c04e1f309d1c8c1be954de3f
Rodion Sibov <rodionsibov@gmail.com>
2021-02-05 14:33:58 +0200

    Update

--- exercises.js
+++ exercises.js
@@ -765,15 +765,22 @@
 // console.log(dropElements([1,2,3,4], function(n) {return n > 5;})) // []
 
 
-function steamrollArray(arr) {
-    return arr.flat(3);
-}
-console.log(steamrollArray([1,[2],[3,[[4]]]])) // [1,2,3,4]
-console.log(steamrollArray([1,{},[3,[[4]]]])) // [1,2,3,4]
+// function steamrollArray(arr) {
+//     return arr.flat(3);
+// }
+// console.log(steamrollArray([1,[2],[3,[[4]]]])) // [1,2,3,4]
+// console.log(steamrollArray([1,{},[3,[[4]]]])) // [1,2,3,4]
 //console.log(Array.isArray([]))
 
 
 
+
+function binaryAgent(str) {
+    return str.split(" ")
+	.map(item => String.fromCharCode(parseInt(item, 2)))
+	.join("");
+}
+console.log(binaryAgent("010000001 010101 0100010 0101010101 11101010101 010 01100101")) 
 
 // if (function() !== "") {
 //     throw new Error("boom")
