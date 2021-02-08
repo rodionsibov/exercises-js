4a2a6a8bcf44a07821706812f400aca7ffec846c
Rodion Sibov <rodionsibov@gmail.com>
2021-02-08 14:22:23 +0200

    Update

--- exercises.js
+++ exercises.js
@@ -840,16 +840,15 @@
 // console.log(bob.getFullName());
 
 
-function orbitalPeriod(arr) {
-    const GM = 398600.4418;
-    const earthRadius = 6367.4447;
-
-    return arr.map(item => ({
-	name: item.name,
-	orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM))
-    }))
-}
-console.log(orbitalPeriod([{name: "sputnik", avgAlt: 35873.5553}])) // [{name: "sputnik", orbitalPeriond: 86400}]
+// function orbitalPeriod(arr) {
+//     const GM = 398600.4418;
+//     const earthRadius = 6367.4447;
+//     return arr.map(item => ({
+// 	name: item.name,
+// 	orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM))
+//     }))
+// }
+// console.log(orbitalPeriod([{name: "sputnik", avgAlt: 35873.5553}])) // [{name: "sputnik", orbitalPeriond: 86400}]
 
 
 
