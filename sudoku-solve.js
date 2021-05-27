0b92e4fe28ed23444cae042a85d4f5a49f046f46
Rodion Sibov <rodionsibov@gmail.com>
2021-05-27 18:53:06 +0300

    first commit

---
+++ sudoku-solve.js
@@ -0,0 +1,21 @@
+const input = [
+    ['4', '.', '1', '.'],
+    ['.', '2', '.', '.'],
+    ['.', '.', '3', '.'],
+    ['.', '.', '2', '.'],
+]
+
+function solveSudoku(board) {
+    const size = 4
+    const boxSize = 4
+
+    const solve = () => {
+
+    }
+    
+    solve()
+    return board
+}
+
+console.table(input);
+console.table(solveSudoku(input));
