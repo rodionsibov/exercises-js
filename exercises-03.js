f9cb259a026facee518f08ef6aa0533c94429d7b
Rodion Sibov <rodionsibov@gmail.com>
2021-06-29 10:59:27 +0300

    Initial commit

---
+++ exercises-03.js
@@ -0,0 +1,9 @@
+// Fibonacci sequence
+let number = 10
+let n1 = 0, n2 = 1, nextTerm;
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
