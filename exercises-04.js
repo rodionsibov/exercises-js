ea5aa4d6fdff52b0396a502077a3358cafea15d3
Rodion Sibov <rodionsibov@gmail.com>
2021-10-19 21:50:22 +0300

    update

--- exercises-04.js
+++ exercises-04.js
@@ -5,7 +5,24 @@ function recursiveSummation(inputNumber) {
     return inputNumber + recursiveSummation(inputNumber - 1)
 }
 
-let result1 = recursiveSummation(3)
-let result2 = recursiveSummation(7)
+const result1 = recursiveSummation(3)
+const result2 = recursiveSummation(7)
 
+function findBinary(decimal, result) {
+    if (decimal === 0) {
+        return result
+    }
+    result = decimal % 2 + result
+    return findBinary(decimal / 2, result)
+}
 
+const result3 = findBinary(234, '')
+
+function reverseString(input) {
+    if (input === '') {
+        return ''
+    }
+    return reverseString(input.substring(1)) + input.charAt(0)
+}
+
+const result4 = reverseString('olleh')
