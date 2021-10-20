eefc633e3ffccf46911895f8ba735deb773c944a
Rodion Sibov <rodionsibov@gmail.com>
2021-10-20 12:14:44 +0300

    fix: convert to an integer number

--- exercises-04.js
+++ exercises-04.js
@@ -12,7 +12,7 @@ function findBinary(decimal, result) {
     if (decimal === 0) {
         return result
     }
-    result = decimal % 2 + result
+    result = parseInt(decimal) % 2 + result
     return findBinary(decimal / 2, result)
 }
 
