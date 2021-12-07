69c51d88f47024a49a93f3d627a67cbd3d5c859b
Rodion Sibov <rodionsibov@gmail.com>
2021-12-07 22:07:43 +0200

    update

--- exercises-05.js
+++ exercises-05.js
@@ -26,12 +26,25 @@ const path = require('path')
 //     }
 //     console.log('the file has appended');
 // })
+const data = {
+    title: 'name', id: Date.now()
+}
 
 const writeFileAsync = async () => {
-    return new Promise((resolve, reject) => fs.writeFile(path.resolve(__dirname, 'test.txt'), (err) => {
+    return new Promise((resolve, reject) => fs.writeFile(path.resolve(__dirname, 'test.txt'), JSON.stringify(data), (err) => {
         if (err) {
-           return reject(err.message)
+            return reject(err.message)
         }
         resolve()
     }))
 }
+writeFileAsync()
+
+// const appendFileAsync = async (path, data) => {
+//     return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+//         if (err) {
+//             return reject(err.message)
+//         }
+//         resolve()
+//     }))
+// }
