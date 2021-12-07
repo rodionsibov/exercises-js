88d3274f15ea44eafad3e684bdc1e243591513f6
Rodion Sibov <rodionsibov@gmail.com>
2021-12-07 16:17:09 +0200

    update

--- exercises-05.js
+++ exercises-05.js
@@ -1,3 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 
+console.log('start');
+
+fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
+    if (err) {
+        throw(err)
+    }
+    console.log('go');
+})
+
+fs.rmdir(path.resolve(__dirname, 'dir'), (err) => {
+    if(err) {
+        throw(err)
+    }
+})
