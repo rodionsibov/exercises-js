aec208807e7be4fee2b3afa81163f744d96bce8e
Rodion Sibov <rodionsibov@gmail.com>
2021-12-07 21:48:23 +0200

    update

--- exercises-05.js
+++ exercises-05.js
@@ -20,9 +20,18 @@ const path = require('path')
 //     console.log('the file has written');
 // })
 
-fs.appendFile(path.resolve(__dirname, 'test.txt'), '5 sdfsdfsd 7 78s9 9', (err) => {
-    if (err) {
-        throw (err)
-    }
-    console.log('the file has appended');
-})
+// fs.appendFile(path.resolve(__dirname, 'test.txt'), '5 sdfsdfsd 7 78s9 9', (err) => {
+//     if (err) {
+//         throw (err)
+//     }
+//     console.log('the file has appended');
+// })
+
+const writeFileAsync = async () => {
+    return new Promise((resolve, reject) => fs.writeFile(path.resolve(__dirname, 'test.txt'), (err) => {
+        if (err) {
+           return reject(err.message)
+        }
+        resolve()
+    }))
+}
