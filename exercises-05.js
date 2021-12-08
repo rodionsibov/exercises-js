ac885689e8cf1fa14461e3b2149ab5763b48c074
Rodion Sibov <rodionsibov@gmail.com>
2021-12-08 09:52:23 +0200

    update

--- exercises-05.js
+++ exercises-05.js
@@ -26,25 +26,39 @@ const path = require('path')
 //     }
 //     console.log('the file has appended');
 // })
+
 const data = {
-    title: 'name', id: Date.now()
+    title: 'Design Patterns', id: Date.now()
 }
 
-const writeFileAsync = async () => {
-    return new Promise((resolve, reject) => fs.writeFile(path.resolve(__dirname, 'test.txt'), JSON.stringify(data), (err) => {
+const writeFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
         if (err) {
             return reject(err.message)
         }
         resolve()
     }))
 }
-writeFileAsync()
+const appendFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.appendFile(path, data, (err) => {
+        if (err) {
+            return reject(err.message)
+        }
+        resolve()
+    }))
+}
 
-// const appendFileAsync = async (path, data) => {
-//     return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
-//         if (err) {
-//             return reject(err.message)
-//         }
-//         resolve()
-//     }))
-// }
+const readFileAsync = async (path) => {
+    return new Promise((resolve, reject) => fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+            return reject(err.message)
+        }
+        resolve(data)
+    }))
+}
+
+writeFileAsync(path.resolve(__dirname, 'test.txt'), JSON.stringify(data))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), '\n133'))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), ' 3454'))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), ' 1sfsd3'))
+    .catch(err => console.log(err))
