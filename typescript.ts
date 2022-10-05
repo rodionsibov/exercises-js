540f9c3d1c93b9b381673433b4f9e0d9bd9090c1
Rodion Sibov <rodionsibov@gmail.com>
2022-10-05 15:27:04 +0300

    First commit

---
+++ typescript.ts
@@ -0,0 +1,33 @@
+let hello: string = "hello";
+
+const getFullNamed = (name: string, surname: string): string[] => {
+  return [name, surname];
+};
+
+console.log(getFullNamed("13", "33"));
+
+interface UserInterface {
+  name: string;
+  age?: number;
+  getMessages?(): string
+}
+
+// const user: UserInterface = {
+//   name: "bob",
+//   age: 23,
+// };
+
+const user2: UserInterface = {
+  name: "bob",
+};
+
+console.log(user2.name)
+
+
+let username: string = 'alex'
+
+let pageName: string | number = '1'
+
+let errorMessage: string | null = null
+
+let user: UserInterface | null = null
