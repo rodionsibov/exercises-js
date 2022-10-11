2333f52a01525e7dda6f4094b1771d17dca9d96a
Rodion Sibov <rodionsibov@gmail.com>
2022-10-11 10:28:06 +0300

    update

--- typescript.ts
+++ typescript.ts
@@ -65,3 +65,8 @@ let numericPageNumber: number = (pageNumber as unknown) as number;
 
 const someElement = document.querySelector("foo") as HTMLInputElement;
 console.log(someElement.value);
+
+someElement.addEventListener("blur", (event) => {
+  const target = event.target as HTMLInputElement;
+  console.log("event", target.value);
+});
