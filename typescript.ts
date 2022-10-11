ddec06fd652d604b55a2384b95ff6687f5d457be
Rodion Sibov <rodionsibov@gmail.com>
2022-10-11 10:09:09 +0300

    Type assertion

--- typescript.ts
+++ typescript.ts
@@ -55,7 +55,11 @@ const doSomething2 = (): never => {
 let vAny: any = 10
 let vUnknown: unknown = 10
 let s1: string = vAny;
-let s2: string = vUnknown;
+let s2: string = vUnknown as string;
 
-console.log(vAny.foo())
-console.log(vUnknown.foo())
+let pageNumber: string = "1";
+let numericPageNumber: number = (pageNumber as unknown) as number;
+
+// console.log(vAny.foo())
+// console.log(vUnknown.foo())
+
