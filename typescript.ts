62c6ba6f9ecca9f2c53a98534f21d9da3b851848
Rodion Sibov <rodionsibov@gmail.com>
2022-10-11 14:11:28 +0300

    Enums in typescript

--- typescript.ts
+++ typescript.ts
@@ -142,4 +142,22 @@ const user6: UserInterface3<string[]> = {
 
 // const result = addId<UserInterface3>(user5);
 
+const statuses = {
+  notStarted: 0,
+  inProgress: 1,
+  done: 2,
+};
+
+enum Status {
+  NotStarted,
+  InProgress,
+  Done,
+}
+
+let notStartedStatus: Status = Status.NotStarted
+
+notStartedStatus = Status.Done 
+
+console.log(Status.InProgress)
+
 
